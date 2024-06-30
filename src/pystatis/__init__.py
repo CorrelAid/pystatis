13c5372cd276d3794bf682cd0fca98e21820ecff
Michael Aydinbas <michael.aydinbas@gmail.com>
2024-06-30 22:16:34 +0200

    increase version to 0.3.0

--- src/pystatis/__init__.py
+++ src/pystatis/__init__.py
@@ -15,7 +15,7 @@ from pystatis.find import Find
 from pystatis.helloworld import logincheck, whoami
 from pystatis.table import Table
 
-__version__ = "0.2.0"
+__version__ = "0.3.0"
 
 __all__ = [
     "clear_cache",
